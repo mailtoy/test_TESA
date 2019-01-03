14eb59d4cc1810dffa13a32d4b4b9fde8258c171
mailtoy <mai_kms11@hotmail.com>
2019-01-03 16:45:26 +0700

    add ssl

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3999
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
@@ -16,7 +16,7 @@ app.listen(port)
 function reply(reply_token) {
     let headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer {xxxxxxx}'
+        'Authorization': 'Bearer {Akqq1LRCE4ZABFHwCIHeQTuv//SnUJ8DFjEbdiU2rNwjQ/hDLseawVRk21/5GoQSCzb8Qbi6am62mQapoL0rNcX7gVJfCvtGokjmUYl0WQjwNVFisKCAfZEHAT6Udzezib0ik54KLeOJaEwvC9dk3wdB04t89/1O/w1cDnyilFU=}'
     }
     let body = JSON.stringify({
         replyToken: reply_token,
@@ -37,3 +37,27 @@ function reply(reply_token) {
         console.log('status = ' + res.statusCode);
     });
 }
+
+const privateKey = fs.readFileSync(
+    "/etc/letsencrypt/live/tesa.thitgorn.com/privkey.pem",
+    "utf8"
+  );
+  const certificate = fs.readFileSync(
+    "/etc/letsencrypt/live/tesa.thitgorn.com/cert.pem",
+    "utf8"
+  );
+  const ca = fs.readFileSync(
+    "/etc/letsencrypt/live/tesa.thitgorn.com/chain.pem",
+    "utf8"
+  );
+
+  const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+  };
+
+  const httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(4000, () => {
+    console.log("HTTPS Server running on port 4000");
+  });
